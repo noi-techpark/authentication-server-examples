4de3209a0fc1ceb900f09b7534715b3f57f7be95
Martin Malfertheiner <martin.malfertheiner@gmail.com>
2020-01-07 09:04:13 +0100

    inital spring setup

---
+++ src/main/java/it/noi/authexamplespringclienttoserver/AuthExampleSpringClientToServerApplication.java
@@ -0,0 +1,13 @@
+package it.noi.authexamplespringclienttoserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthExampleSpringClientToServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthExampleSpringClientToServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/noi/authexamplespringclienttoserver/AuthExampleSpringClientToServerApplicationTests.java
@@ -0,0 +1,13 @@
+package it.noi.authexamplespringclienttoserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthExampleSpringClientToServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
