b1a86de1d2d32328760d524cbdfe163fa16484e8
Martin Malfertheiner <martin.malfertheiner@gmail.com>
2020-01-08 10:19:32 +0100

    restructure repository

---
+++ spring-resource-server/src/main/java/it/noi/resourceserver/AuthExampleSpringResourceServerApplication.java
@@ -0,0 +1,13 @@
+package it.noi.resourceserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthExampleSpringResourceServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthExampleSpringResourceServerApplication.class, args);
+	}
+
+}
---
+++ spring-resource-server/src/test/java/it/noi/resourceserver/AuthExampleSpringResourceServerApplicationTests.java
@@ -0,0 +1,13 @@
+package it.noi.resourceserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthExampleSpringResourceServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
