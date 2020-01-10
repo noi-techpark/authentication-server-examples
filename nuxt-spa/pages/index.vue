93e42a54f84234f6f148f9f08cf8c18a89b4b65e
Martin Malfertheiner <martin.malfertheiner@gmail.com>
2020-01-10 10:32:45 +0100

    style index page

--- nuxt-spa/pages/index.vue
+++ nuxt-spa/pages/index.vue
@@ -1,9 +1,7 @@
 <template>
-  <div class="container">
-    <div>
-      <h1>
-        Example SPA App
-      </h1>
+  <div class="p-5 flex justify-center bg-gray-100">
+    <div class="mx-2 my-4 xl:mx-0 p-5 border-3 border-black bg-white">
+      <h1>You are not logged in!</h1>
     </div>
   </div>
 </template>
